b8b8b11302ecc331f701340766c1a522a4b7961b
shng6444 <shng6444@uni.sydney.edu.au>
2023-02-23 05:03:48 +1100

    second commit

--- main/java/app/helloworld/Main.java
+++ main/java/app/helloworld/Main.java
@@ -3,10 +3,8 @@ import app.helloworld.view.Window;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-// import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 
 public class Main extends Application {
 
